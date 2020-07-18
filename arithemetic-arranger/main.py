cfde186f95fbd24462d1c2eec6a5dba35ffb648b
nimith <nimithth@gmail.com>
2020-07-18 14:10:50 +0530

    First commit

---
+++ arithemetic-arranger/main.py
@@ -0,0 +1,8 @@
+# This entrypoint file to be used in development. Start by reading README.md
+from arithmetic_arranger import arithmetic_arranger
+
+
+
+arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"])
+
+
